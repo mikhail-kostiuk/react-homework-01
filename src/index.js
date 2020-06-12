ee8ca3236fbd153c4a988cda6d4e6b4251cba737
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-06-12 13:27:16 +0300

    Task-08

--- src/index.js
+++ src/index.js
@@ -16,6 +16,57 @@ const nextPresidents = [
   { firstName: 'Millard', lastName: 'Fillmore', presidentIndex: 13 }
 ];
 
+const serverResponse = [
+  {
+    'id': '666958530825467',
+    'title': 'Friday open-air party w/ Lucarelli, Mihas and Yarik JR | Hide',
+    'place': 'Hide',
+    'date': '2020-06-12T20:00:00.000Z'
+  },
+  {
+    'id': '786185895252176',
+    'title': 'Захист скверу імені Чкалова',
+    'place': 'Сквер Им. Чкалова',
+    'date': '2020-06-10T09:00:00.000Z'
+  },
+  {
+    'id': '623921328209118',
+    'title': 'Живая музыка на летней террасе',
+    'place': 'От Заката до Рассвета',
+    'date': '2020-06-14T17:00:00.000Z'
+  },
+  {
+    'id': '909559356190295',
+    'title': 'Amer (2009)',
+    'place': 'Кіноклуб Кіноха',
+    'date': '2020-06-13T15:00:00.000Z'
+  },
+  {
+    'id': '589272605321022',
+    'title': 'В парк Межигорье на теплоходе',
+    'place': 'Причал №6, Почтовая пл.',
+    'date': '2020-06-13T07:45:00.000Z'
+  }
+];
+
+function getDaytime(date) {
+  const hour = new Date(date).getHours();
+
+  if (hour >= 5 && hour < 11) {
+    return 'Утро';
+  }
+
+  if (hour >= 11 && hour < 17) {
+    return 'День';
+  }
+
+  if (hour >= 17 && hour < 21) {
+    return 'Вечер';
+  }
+
+  return 'Ночь';
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <ul>
@@ -23,11 +74,13 @@ ReactDOM.render(
       <li>John Adams</li>
       <li>Thomas Jefferson</li>
     </ul>
+
     <ol start="4">
       <li>James Madison</li>
       <li>James Monroe</li>
       <li>John Quincy Adams</li>
     </ol>
+
     <ul
       style={{
         backgroundColor: '#ddd',
@@ -40,6 +93,7 @@ ReactDOM.render(
         <li key={president}>{president}</li>
       ))}
     </ul>
+
     <ul>
       {nextPresidents.map(president => {
         return president.presidentIndex % 2 ? (
@@ -48,6 +102,26 @@ ReactDOM.render(
           >{`${president.lastName}, ${president.firstName}, ${president.presidentIndex}-th`}</li>
         ) : null;
       })}
+    </ul>
+
+    <ul className="events">
+      {serverResponse
+        .sort((a, b) => new Date(a.date) - new Date(b.date))
+        .map(event => {
+          const date = new Date(event.date);
+
+          return (
+            <li style={date < Date.now() ? { opacity: '0.5' } : null}>
+              <a href={`https://www.facebook.com/events/${event.id}/`}>
+                {event.title}
+              </a>
+              <span>
+                {getDaytime(event.date)}, {date.toLocaleString()}
+              </span>
+              <span>{event.place}</span>
+            </li>
+          );
+        })}
     </ul>
   </React.StrictMode>,
   document.getElementById('root')
